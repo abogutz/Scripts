a2d37ceddae22a6551a090eee1db9a2d7cf9ce9f
tiffyl <tiffyyleung@gmail.com>
2020-03-31 14:44:09 -0700

    add checkPseudogenome function

--- MEA_functions.sh
+++ MEA_functions.sh
@@ -6,11 +6,49 @@ CURRENT_DIRECTORY=$(pwd)
 DIPLOID_GENOME_DIR=$GENOME_DIR/diploid #GENOME_DIR determined from the start of the script using GENOME_BUILD
 HAPLOID_GENOME_DIR=$GENOME_DIR/haploid
 
-function setPseudogenomes () {
-  NEED_HAPLOID=true
-  HAP1_REFGENOME=""
-  HAP2_REFGENOME=""
+function checkPseudogenome() {
+  if $ALLELE_SPECIFIC; then
 
+    CROSS_LIST=$(awk '{if ($3!="" || $4!="") {print $3"_"$4}}' $INPUT_FILE | sort | uniq) #create array of crosses
+    EXIT_SCRIPT=false
+
+    for CROSS in $CROSS_LIST; do
+      STRAIN_1=${CROSS//_*}
+      STRAIN_2=${CROSS##*_}
+
+      if [[ -z $STRAIN_1 || -z $STRAIN_2 ]]; then
+        echo "One of your samples only have 1 strain entered. \nPlease ensure both strains are entered for allele specific pipeline"
+        EXIT_SCRIPT=true
+        continue #continue to next iteration of cross
+      fi
+
+      MAKE_GENOME=true
+      
+      for DIP in $DIPLOID_GENOME_DIR/*; do #searching for folder with name of both strains
+        if [[ $DIP == *$STRAIN_1* && $DIP == *$STRAIN_2* ]]; then
+          MAKE_GENOME=false
+          checkFileExits $HAPLOID_GENOME_DIR/$STRAIN_1/$STRAIN_1".fa.refmap"
+          checkFileExits $HAPLOID_GENOME_DIR/$STRAIN_2/$STRAIN_2".fa.refmap"
+
+          echo "Reference genome for $CROSS is found"
+          break #exit the loop of iterating thru all the diploid genomes for this combo of cross 
+        fi
+      done
+
+      if $MAKE_GENOME; then
+        echo "Diploid genome was not found for $CROSS. \nPlease use provided CreateDipPseudoGenome.sh to obtain diploid pseudogenome."
+        EXIT_SCRIPT=true
+      fi
+      
+    done
+
+    if $EXIT_SCRIPT; then
+      exit #exit script to ensure input is formatted correctly before running the whole script
+    fi
+  fi  
+}
+
+function setPseudogenome () {
   for code in $CODE_ARRAY; do
     SRACODE=$code
     NAME=$(grep -e $SRACODE $INPUT_FILE | cut -f2)
@@ -22,50 +60,21 @@ function setPseudogenomes () {
       continue #dont continue the function for this SRACODE entry (move on to next code)
     fi
     
-    for dip_strains_dir in $DIPLOID_GENOME_DIR/*; do #searching for folder with name of both strains
-      if [[ $dip_strains_dir == *$STRAIN_1* && $dip_strains_dir == *$STRAIN_2* ]]; then
-        GENOME_FILE=$dip_strains_dir/*.fa
-        STAR_GENOME_DIR=$dip_strains_dir/$STRAIN_1"_"$STRAIN_2"-STAR"
-        BOWTIE2_GENOME_DIR=$dip_strains_dir
-        NEED_HAPLOID=false
+    for DIP in $DIPLOID_GENOME_DIR/*; do #searching for folder with name of both strains
+      if [[ $DIP == *$STRAIN_1* && $DIP == *$STRAIN_2* ]]; then
+        GENOME_FILE=$DIP/*.fa
+        STAR_GENOME_DIR=$DIP/*"-STAR"
+        BOWTIE2_GENOME_DIR=$DIP
 
-        checkFileExits $HAPLOID_GENOME_DIR/$STRAIN_1".fa.refmap"
-        checkFileExits $HAPLOID_GENOME_DIR/$STRAIN_2".fa.refmap"
+        STRAIN_1_REF=$HAPLOID_GENOME_DIR/$STRAIN_1/$STRAIN_1".fa.refmap"    
+        STRAIN_2_REF=$HAPLOID_GENOME_DIR/$STRAIN_2/$STRAIN_2".fa.refmap"
 
-        echo "Reference genome for $NAME is found"
+        echo "Reference genome for $NAME is set to $DIP"
+        break
       fi
     done
 
-    if $NEED_HAPLOID; then
-      for hap_genome in $HAPLOID_GENOME_DIR/*.fa; do
-        if [[ $hap_genome == *$STRAIN_1*]]; then
-          echo "Haploid genome for $STRAIN_1 exists"
-          HAP1_REFGENOME=$hap_genome
-          checkFileExits $hap_genome".refmap"
-          
-        elif [[ $hap_genome == *$STRAIN_2* ]]; then
-          echo "Haploid genome for $STRAIN_2 exists"
-          HAP2_REFGENOME=$hap_genome
-          checkFileExits $hap_genome".refmap"
-        fi
-      done
-    fi
-
-    if [[ $NEED_HAPLOID && $HAP1_REFGENOME != "" &&  $HAP2_REFGENOME != ""]]; then
-      mkdir -p $DIPLOID_GENOME_DIR/$STRAIN_1"_"$STRAIN_2
-      cat $HAP1_REFGENOME $HAP2_REFGENOME > $DIPLOID_GENOME_DIR/$STRAIN_1"_"$STRAIN_2/$STRAIN_1"_"$STRAIN_2".fasta"
-      REFGENOME=$STRAIN_1"_"$STRAIN_2".fasta"
-      echo "Reference genome is $REFGENOME"
-      GENOME_FILE=$dip_strains_dir/*.fa
-      STAR_GENOME_DIR=$dip_strains_dir/$STRAIN_1"_"$STRAIN_2"-STAR"
-      BOWTIE2_GENOME_DIR=$dip_strains_dir
-            
-    else
-      echo -e "Cannot find fasta file for diploid or haploid genome for at least one strain.\nPlease create genome fasta file (using alea createGenome) for missing strain(s) before running script again.\nExiting script..."
-      exit
-    fi
-
-    
+  done
 }
 
 function allespecUnpack () { #working on bam that has already aligned to the pseudogenome (need to run this twice, once with each strain
