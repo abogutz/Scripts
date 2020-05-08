0b76e5ce68e450df3247cd1ddcd443d228cfe659
tiffyl <tiffyyleung@gmail.com>
2020-05-08 11:43:51 -0700

    fixed spaces to tabs

--- CreateDipPseudoGenome.sh
+++ CreateDipPseudoGenome.sh
@@ -20,32 +20,32 @@ STRAIN_LIST="129S1 B6NJ C57BL6J SvImJ"
 OPTIONS="1:2:g:"
 
 if ( ! getopts $OPTIONS opt); then
-  echo -e "USAGE:\t $(basename $0) -1 <STRAIN_1> -2 <STRAIN_2>"
-  exit 1
+	echo -e "USAGE:\t $(basename $0) -1 <STRAIN_1> -2 <STRAIN_2>"
+	exit 1
 fi
 
 while getopts $OPTIONS opt; do
-  case $opt in
-    1) 
-      STRAIN_1=${OPTARG}
-      ;;
-    2) 
-      STRAIN_2=${OPTARG}
-      ;;
-    g)
-      GENOME_DIR=${OPTARG}
-      ;;
-    \?)
-      echo -e "ERROR: Invalid Option!" >&2
-      exit 1
-      ;;
-  esac
+	case $opt in
+		1) 
+			STRAIN_1=${OPTARG}
+			;;
+		2) 
+			STRAIN_2=${OPTARG}
+			;;
+		g)
+			GENOME_DIR=${OPTARG}
+			;;
+		\?)
+			echo -e "ERROR: Invalid Option!" >&2
+			exit 1
+			;;
+	esac
 done
 
 #Check to ensure both strains are entered
 if [[ -z $STRAIN_1 || -z $STRAIN_2 ]]; then
-  echo "Please enter 2 strains to create pseudogenome."
-  exit
+	echo "Please enter 2 strains to create pseudogenome."
+	exit
 fi
 
 #Ensure the strains are ones that we support
@@ -53,35 +53,35 @@ FAIL_STRAIN_1==true
 FAIL_STRAIN_2==true
 
 for strain in $STRAIN_LIST; do
-  if [[ $FAIL_STRAIN_1 && $strain == $STRAIN_1 ]];
-    FAIL_STRAIN_1=false
-    echo -e "$STRAIN_1 supported."
-  elif [[ $FAIL_STRAIN_2 && $strain == $STRAIN_2 ]];
-    FAIL_STRAIN_2=false
-    echo -e "$STRAIN_2 supported."
-  fi
+	if [[ $FAIL_STRAIN_1 && $strain == $STRAIN_1 ]];
+		FAIL_STRAIN_1=false
+		echo -e "$STRAIN_1 supported."
+	elif [[ $FAIL_STRAIN_2 && $strain == $STRAIN_2 ]];
+		FAIL_STRAIN_2=false
+		echo -e "$STRAIN_2 supported."
+	fi
 done
 
 if [[ $FAIL_STRAIN_1 || $FAIL_STRAIN_2 ]]
-  echo -e "At least one of your strains is not found. \nPlease double check spelling of the strains. \nIf the strain is not supported, please contact..."
-  exit
+	echo -e "At least one of your strains is not found. \nPlease double check spelling of the strains. \nIf the strain is not supported, please contact..."
+	exit
 fi
 
 #Ensure we are not creating the same diploid pseudogenome
 for DIP in $DIPLOID_GENOME_DIR/*; do #searching for folder with name of both strains
-  if [[ $DIP == *$STRAIN_1* && $DIP == *$STRAIN_2* ]]; then
-    echo "Reference diploid pseudogenome is found"
-    exit 
-  fi
+	if [[ $DIP == *$STRAIN_1* && $DIP == *$STRAIN_2* ]]; then
+		echo "Reference diploid pseudogenome is found"
+		exit 
+	fi
 done
 
 #Find the corresponding haploid fasta files
 for HAP in $HAPLOID_GENOME_DIR/*.fa; do
-  if [[ $HAP == *$STRAIN_1*]]; then
-    HAP1_REFGENOME=$HAP
-  elif [[ $HAP == *$STRAIN_2* ]]; then
-    HAP2_REFGENOME=$HAP
-  fi
+	if [[ $HAP == *$STRAIN_1*]]; then
+		HAP1_REFGENOME=$HAP
+	elif [[ $HAP == *$STRAIN_2* ]]; then
+		HAP2_REFGENOME=$HAP
+	fi
 done
 
 #Create diploid pseudogenome
