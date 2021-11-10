7bc69707e9db0deeda1ab27da2e8e09270375dc4
abogutz <37719032+abogutz@users.noreply.github.com>
2021-11-10 12:09:22 -0800

    Update runCC.sh

--- runCC.sh
+++ runCC.sh
@@ -1,17 +1,25 @@
 #! /bin/bash
-#SBATCH --account=<Group Name>            # required
+#SBATCH --account=<Group Name>            # required (format def-name)
 #SBATCH --ntasks=8                        # number of MPI processes
-#SBATCH --mem-per-cpu=12G                 # memory; default unit is megabytes
+#SBATCH --mem-per-cpu=4G                 # memory; default unit is megabytes
 #SBATCH --time=01-12:00                   # time (DD-HH:MM)
 #SBATCH --mail-user=<email address>
 #SBATCH --mail-type=ALL
 
 
 
-# Modify these to whatever you want
-INPUT_FILE=""
+# Modify these to whatever you want. Should match up with SBATCH submission parameters above.
+SRA_FILE=""
 GENOME="mm10"
+THREADS=8
+MEM_THREAD="4G"
 
 # Many other options exist for MasterDAT.sh, run with -h option to see all of them
 
-/project/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -i $INPUT_FILE -g $GENOME
+~/project/scripts/MasterDAT/MasterDAT.sh -i $SRA_FILE -g $GENOME -t $THREADS -m $MEM_THREAD 
+
+#~/project/scripts/MasterDAT/MasterDAT.sh -t $THREADS -m $MEM_THREAD -f ./Fastq -g $GENOME # Fastq input - will use .fastq.gz files located in ./Fastq (can change). Name format is very important
+
+#~/project/scripts/MasterDAT/MasterDAT.sh -t $THREADS -m $MEM_THREAD -b -g $GENOME # BAM file input - will find ALL bam files in subdirectories of current location (one level down)
+
+#~/project/scripts/MasterDAT/MasterDAT.sh -t $THREADS -m $MEM_THREAD -i $SRA_FILE -g $GENOME -F # Only produce fastq files after downloading
