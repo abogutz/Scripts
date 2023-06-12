e2e7a4d71a94dace1f0042431d778e542e12f30b
abogutz <37719032+abogutz@users.noreply.github.com>
2023-06-12 14:14:13 -0700

    Update runCC.sh

--- runCC.sh
+++ runCC.sh
@@ -13,13 +13,15 @@ SRA_FILE=""
 GENOME="mm10"
 THREADS=8
 MEM_THREAD="4G"
+TEMP_DIR=$HOME"/scratch/"$SLURM_JOB_ID"/"
+mkdir $TEMP_DIR
 
 # Many other options exist for MasterDAT.sh, run with -h option to see all of them
 
-$HOME/projects/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -i $SRA_FILE -g $GENOME -t $THREADS -m $MEM_THREAD 
+$HOME/projects/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -d $TEMP_DIR -t $THREADS -m $MEM_THREAD -f ./Fastq -g $GENOME # Fastq input - will use .fastq.gz files located in ./Fastq (can change). Name format is very important
 
-#$HOME/projects/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -t $THREADS -m $MEM_THREAD -f ./Fastq -g $GENOME # Fastq input - will use .fastq.gz files located in ./Fastq (can change). Name format is very important
+#$HOME/projects/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -d $TEMP_DIR -t $THREADS -m $MEM_THREAD -b -g $GENOME # BAM file input - will find ALL bam files in subdirectories of current location (one level down)
 
-#$HOME/projects/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -t $THREADS -m $MEM_THREAD -b -g $GENOME # BAM file input - will find ALL bam files in subdirectories of current location (one level down)
+#$HOME/projects/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -d $TEMP_DIR -t $THREADS -m $MEM_THREAD -i $SRA_FILE -g $GENOME -F # Only produce fastq files after downloading
 
-#$HOME/projects/def-mlorincz/scripts/MasterDAT/MasterDAT.sh -t $THREADS -m $MEM_THREAD -i $SRA_FILE -g $GENOME -F # Only produce fastq files after downloading
+rm -r $TEMP_DIR
